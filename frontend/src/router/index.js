7815645a1afd8278c4cf704b2f3ca76a015c33b1
mzumi <masazumi.m@gmail.com>
2018-12-16 18:46:15 +0900

    Revert "Vue.js を使用して画面を作成"

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,30 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
-import Landing from '@/components/Landing'
-import Problem from '@/components/Problem'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-const parseProblemNumber = (route) => {
-  return {
-    number: parseInt(route.params.number)
-  }
-}
-
 export default new Router({
-  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'Landing',
-      component: Landing
-    },
-    {
-      path: '/problem/:number',
-      name: 'Problem',
-      props: parseProblemNumber,
-      component: Problem
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
